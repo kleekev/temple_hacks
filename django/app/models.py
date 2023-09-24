69d84a956ca35f861f048424b0e90fea54eb9a4c
fc387@drexel.edu <fc387@drexel.edu>
2023-09-24 01:21:29 -0400

    sorry it took so long

--- django/app/models.py
+++ django/app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    id=models.IntegerField(primary_key=True)
     username=models.CharField(max_length=260)
     password=models.CharField(max_length=260)
+
     
